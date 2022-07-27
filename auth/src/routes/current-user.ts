eadf8a78094b3cda336afadfe1c6ead9b3051ed7
babaliauskas <l.babaliauskas@gmail.com>
2022-07-26 20:09:26 -0400

    added couple middlewares

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,18 +1,11 @@
-import express from "express";
-import jwt from "jsonwebtoken";
+import express, {Request, Response} from "express";
+
+import {currentUser} from "../middlewares/current-user";
 
 const router = express.Router()
 
-router.get('/api/users/currentuser', (req, res) => {
-    if (!req.session?.jwt) {
-        return res.send({currentUser: null})
-    }
-    try {
-        const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!)
-        res.send({currentUser: payload})
-    } catch (err) {
-        res.send({currentUser: null})
-    }
+router.get('/api/users/currentuser', currentUser, (req: Request, res: Response) => {
+    res.send({currentUser: req.currentUser || null})
 })
 
 export {router as currentUserRouter}
